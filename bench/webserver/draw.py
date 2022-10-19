f27b3779f1a828db1bc66d3690ec15913d3097bd
Shravan Narayan <shravanrn@gmail.com>
2022-10-19 12:32:56 -0400

    Fix graph plot

--- bench/webserver/draw.py
+++ bench/webserver/draw.py
@@ -27,34 +27,34 @@ print(t2)
 
 data = [
     list(np.divide(t1, t1)),
-    list(np.divide(t2, t1)),        
+    list(np.divide(t2, t1)),
 ]
 
 print(data)
-X = np.arange(len(l1))    
+X = np.arange(len(l1))
 fig, ax = plt.subplots(ncols=1, nrows=1)
 delta = 0.35
-ax.bar(X, data[0], color = 'g', width = delta, hatch='/')
-ax.bar(X + delta, data[1], color = 'r', width = delta, hatch='\\')
+ax.bar(X, data[0], color = '#FFFFBF', width = delta, edgecolor = "black")
+ax.bar(X + delta, data[1], color = '#2B83BA', width = delta, edgecolor = "black")
 
 
 ax.text(-1.3, 1.08, str("TPS\nNative"), color='black')
-    
+
 
 for i, v in enumerate(t1):
     ax.text(i-delta, 1.08, str("{:.2f}\n kB/s".format(v/1000)), color='black')
-    
-    
-ax.set_ylabel('Normalized Throughput')
+
+
+ax.set_ylabel('Normalized throughput')
 ax.set_xlabel('File Size')
-ax.legend(labels=['Native', 'HFI'], ncol=2, frameon=False, bbox_to_anchor=(1.01, 1.28))
+# ax.legend(labels=['Native', 'HFI'], ncol=2, frameon=False, bbox_to_anchor=(1.01, 1.28))
+plt.legend(labels=['Native', 'HFI'], bbox_to_anchor=(1, 1.5), frameon=False, loc='upper right', ncol=2)
 ax.set_xticks(X+delta/2)
 ax.set_xticklabels(l1)
-ax.set_yticks(np.arange(0, 1.1, 0.1))
+ax.set_yticks(np.arange(0, 1.1, 0.25))
 
-    
-fig.tight_layout()
-fig.set_size_inches(6, 4)
+fig.tight_layout(pad=2.5)
+fig.set_size_inches(6, 3)
 fig.savefig('nginx.png', dpi=100)
 # plt.show()
 
